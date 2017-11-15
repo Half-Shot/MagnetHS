d5d7c726dfa4e4148a03f389660f1dc56058a966
Half-Shot <will@half-shot.uk>
2017-11-15 01:58:14 +0000

    Add test for RestContext

--- HalfShot.MagnetHS/Tests/Services/ClientServerAPIService/RestContext.cs
+++ HalfShot.MagnetHS/Tests/Services/ClientServerAPIService/RestContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using HalfShot.MagnetHS.ClientServerAPIService;
 namespace HalfShot.MagnetHS.Tests.Services.ClientServerAPIService
@@ -9,10 +10,30 @@ namespace HalfShot.MagnetHS.Tests.Services.ClientServerAPIService
     public class TestRestContext
     {
         [TestMethod]
-        public void TestProcessPathParameters()
+        public void TestProcessPathParametersEmpty()
         {
             RestContext context = new RestContext();
-            Assert.IsTrue(true);
+            context.ProcessPathParameters(new Regex("/abc/def/ghi"), "/abc/def/ghi");
+            Assert.AreEqual(0, context.PathParameters.Count);
+        }
+
+        [TestMethod]
+        public void TestProcessPathParametersOne()
+        {
+            RestContext context = new RestContext();
+            context.ProcessPathParameters(new Regex("/abc/(?<key>.+)/ghi"), "/abc/def/ghi");
+            Assert.AreEqual(1, context.PathParameters.Count);
+            Assert.AreEqual("def", context.PathParameters["key"]);
+        }
+
+        [TestMethod]
+        public void TestProcessPathParametersMultiple()
+        {
+            RestContext context = new RestContext();
+            context.ProcessPathParameters(new Regex("/(?<key>.+)/def/(?<key2>.+)"), "/abc/def/ghi");
+            Assert.AreEqual(2, context.PathParameters.Count);
+            Assert.AreEqual("abc", context.PathParameters["key"]);
+            Assert.AreEqual("ghi", context.PathParameters["key2"]);
         }
     }
 }
