4570d12cfde8f60b3775fc59dcca11ebff2d31ca
Will Hunt <will@half-shot.uk>
2017-11-21 09:53:13 +0000

    Add RoomId generator

--- HalfShot.MagnetHS/CommonStructures/RoomID.cs
+++ HalfShot.MagnetHS/CommonStructures/RoomID.cs
@@ -6,9 +6,26 @@ namespace HalfShot.MagnetHS.CommonStructures
 {
     public class RoomID : MatrixCommonId
     {
+        const int LocalpartLength = 18;
         public RoomID(string roomId) : base('!', roomId)
         {
 
+        }
+
+        public static RoomID GenerateRoomId(string domain)
+        {
+            Random random = new Random();
+            string id = "";
+            while(id.Length < LocalpartLength)
+            {
+                int c = 91;
+                while(c >= 91 && c <= 96)
+                {
+                    c = random.Next(65, 122);
+                }
+                id += (char)c;
+            }
+            return new RoomID($"!{id}:{domain}");
         }
     }
 }
