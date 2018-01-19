d7c9def14509fc978eb7bf83c56f4fe3f613975e
Will Hunt <will@half-shot.uk>
2018-01-19 10:15:05 +0000

    Add MsgPack code and tests

---
+++ HalfShot.MagnetHS/DataTransformer/MsgPackDataTransformer.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using MsgPack;
+using MsgPack.Serialization;
+
+namespace HalfShot.MagnetHS.DataTransformer
+{
+    public class MsgPackDataTransformer : IDataTransformer
+    {
+        public byte[] ToBytes(object obj)
+        {
+            var serial = GetSerializer(obj.GetType());
+            return serial.PackSingleObject(obj);
+        }
+
+        public string ConvertToString(object obj)
+        {
+            //Msgpack is a byteformat, so lets just base64 it.
+            return Convert.ToBase64String(ToBytes(obj));
+        }
+
+        public Stream ToStream(object obj)
+        {
+            var serial = GetSerializer(obj.GetType());
+            var stream = new MemoryStream();
+            serial.PackTo(Packer.Create(stream, false), obj);
+            return stream;
+        }
+
+        public T FromString<T>(string data)
+        {
+            byte[] byteData = Convert.FromBase64String(data);
+            return FromBytes<T>(byteData);
+        }
+
+        public T FromBytes<T>(byte[] data)
+        {
+            return (T)GetSerializer(typeof(T)).UnpackSingleObject(data);
+        }
+
+        public T FromStream<T>(TextReader stream)
+        {
+            return FromString<T>(stream.ReadToEnd());
+        }
+
+        private static MessagePackSerializer GetSerializer(Type t)
+        {
+            return SerializationContext.Default.GetSerializer(t);
+        }
+    }
+}
---
+++ HalfShot.MagnetHS/Tests/DataTransformer/MsgPackDataTransformer.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HalfShot.MagnetHS.DataTransformer;
+using System.IO;
+using HalfShot.MagnetHS.CommonStructures;
+
+namespace HalfShot.MagnetHS.Tests.DataTransformer
+{
+    [Serializable]
+    public class TestMsgPackDataTransformerObj
+    {
+        public string TestString { get; set; }
+        public UserID UserId { get; set; }
+    }
+    
+    [TestClass]
+    [TestCategory("DataTransformer")]
+    public class TestMsgPackDataTransformer
+    {
+        [TestMethod]
+        public void TestMsgPackFromStream()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestMsgPackConvertToString()
+        {
+            var obj = new MsgPackDataTransformer().ConvertToString(
+                new TestMsgPackDataTransformerObj()
+                {
+                    TestString = "AWonderfulTest",
+                    UserId = new UserID("@123:abc.com")
+                }
+            );
+            Assert.IsNotNull(obj);
+            Assert.IsTrue(obj.Contains("kq5BV29uZGVyZnVsVGVzdJKnYWJjLmNvbaMxMjM="));
+        }
+
+        [TestMethod]
+        public void TestMsgPackConvertToBytes()
+        {
+            var obj = new MsgPackDataTransformer().ToBytes(
+                new TestMsgPackDataTransformerObj()
+                {
+                    TestString = "AWonderfulTest",
+                    UserId = new UserID("@123:abc.com")
+                }
+            );
+            Assert.IsNotNull(obj);
+            Assert.IsTrue(obj.Length > 0);
+        }
+
+        [TestMethod]
+        public void TestMsgPackConvertToStream()
+        {
+            using (var stream = new MsgPackDataTransformer().ToStream(
+                new TestMsgPackDataTransformerObj()
+                {
+                    TestString = "AWonderfulTest",
+                    UserId = new UserID("@123:abc.com")
+                }
+            )){
+                Assert.IsTrue(stream.Length > 0);
+            }
+        }
+    }
+}
